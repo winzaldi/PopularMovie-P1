bfae5ef86006d3fa314fd7c1b52b022c518ef524
winzaldi <winzaldi@gmail.com>
2017-06-30 17:20:57 +0700

    Change Layout and Service

--- app/src/main/java/com/learn/android/udacity/udacity_popularmovie/MainActivity.java
+++ app/src/main/java/com/learn/android/udacity/udacity_popularmovie/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
